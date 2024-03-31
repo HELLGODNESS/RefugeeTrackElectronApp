3f1960804bd3a6ee480b69f9330150252b3b0c23
junaidraza12 <160530648+junaidraza12@users.noreply.github.com>
2024-03-31 16:47:20 +0500

    hello

--- client/src/components/Pages/AddUser.jsx
+++ client/src/components/Pages/AddUser.jsx
@@ -417,7 +417,6 @@ export default function AddUser() {
             </button>
             <button
               type="submit"
-
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Salva
